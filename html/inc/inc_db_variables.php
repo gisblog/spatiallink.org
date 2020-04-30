8c7f6297f77b3a2d59ad093451cd69be9cb906f6
gisblog <4225996+gisblog@users.noreply.github.com>
2020-04-30 01:58:51 -0400

    var

--- html/inc/inc_db_variables.php
+++
@@ -1,8 +0,0 @@
-<?php
-// $var:
-$varhost='spatiallink.db.3881957.hostedresource.com';
-$vardbtype='mysql';
-$vardb='spatiallink';
-$varuser='spatiallink'; #spatiallinkr ?
-$varpass='XSQLhvp246#';
-?>
